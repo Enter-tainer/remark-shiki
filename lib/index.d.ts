6dd34aed72e110c4ef0f3e441ccaf9dc3e866c7f
true_mogician <925269713@qq.com>
2021-08-19 23:47:50 +0800

    chore: remove lib files

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Plugin } from "unified";
-import { Root } from "mdast";
-export interface RemarkShikiOptions {
-    theme: string;
-    semantic: boolean;
-    skipInline: boolean;
-}
-export declare const remarkShiki: Plugin<[Partial<RemarkShikiOptions>?], Root>;
